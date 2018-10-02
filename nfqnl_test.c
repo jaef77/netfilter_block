9c6984bc7ee335fa4a08290b7a58f5eb43054bc4
lsj <jaef77@naver.com>
2018-10-03 07:20:13 +0900

    Correction of mistakes

--- nfqnl_test.c
+++ nfqnl_test.c
@@ -39,46 +39,49 @@ static struct id_acc print_pkt (struct nfq_data *tb)
 	unsigned char *data;
 
 	ph = nfq_get_msg_packet_hdr(tb);
+	/*
 	if (ph) {
 		id = ntohl(ph->packet_id);
-		// printf("hw_protocol=0x%04x hook=%u id=%u ",
-			ntohs(ph->hw_protocol), ph->hook, id);
+		printf("hw_protocol=0x%04x hook=%u id=%u ", ntohs(ph->hw_protocol), ph->hook, id);
 	}
+	*/
 
 	hwph = nfq_get_packet_hw(tb);
+	/*
 	if (hwph) {
 		int i, hlen = ntohs(hwph->hw_addrlen);
 
-		// printf("hw_src_addr=");
+		printf("hw_src_addr=");
 		for (i = 0; i < hlen-1; i++)
 			printf("%02x:", hwph->hw_addr[i]);
-		// printf("%02x ", hwph->hw_addr[hlen-1]);
+		printf("%02x ", hwph->hw_addr[hlen-1]);
 	}
+	*/
+
 
 	mark = nfq_get_nfmark(tb);
-	if (mark)
-		// printf("mark=%u ", mark);
+	// if (mark)
+	// 	printf("mark=%u ", mark);
 
 	ifi = nfq_get_indev(tb);
-	if (ifi)
-		// printf("indev=%u ", ifi);
+	// if (ifi)
+	//	printf("indev=%u ", ifi);
 
 	ifi = nfq_get_outdev(tb);
-	if (ifi)
-		// printf("outdev=%u ", ifi);
+	// if (ifi)
+	//	printf("outdev=%u ", ifi);
+
 	ifi = nfq_get_physindev(tb);
-	if (ifi)
-		// printf("physindev=%u ", ifi);
+	//if (ifi)
+	//	printf("physindev=%u ", ifi);
 
 	ifi = nfq_get_physoutdev(tb);
-	if (ifi)
-		// printf("physoutdev=%u ", ifi);
+	//if (ifi)
+	//	printf("physoutdev=%u ", ifi);
 
 	ret = nfq_get_payload(tb, &data);
-	if (ret >= 0)
-	{
-		// printf("payload_len=%d ", ret);
-	}
+	// if (ret >= 0)
+	//	printf("payload_len=%d ", ret);
 
 	// fputc('\n', stdout);
 
@@ -162,33 +165,33 @@ int main(int argc, char **argv)
 	host_name = argv[1];
 	host_len = strlen(host_name);
 
-	// printf("opening library handle\n");
+	printf("opening library handle\n");
 	h = nfq_open();
 	if (!h) {
 		fprintf(stderr, "error during nfq_open()\n");
 		exit(1);
 	}
 
-	// printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+	printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
 	if (nfq_unbind_pf(h, AF_INET) < 0) {
 		fprintf(stderr, "error during nfq_unbind_pf()\n");
 		exit(1);
 	}
 
-	// printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+	printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
 	if (nfq_bind_pf(h, AF_INET) < 0) {
 		fprintf(stderr, "error during nfq_bind_pf()\n");
 		exit(1);
 	}
 
-	// printf("binding this socket to queue '0'\n");
+	printf("binding this socket to queue '0'\n");
 	qh = nfq_create_queue(h,  0, &cb, NULL);
 	if (!qh) {
 		fprintf(stderr, "error during nfq_create_queue()\n");
 		exit(1);
 	}
 
-	// printf("setting copy_packet mode\n");
+	printf("setting copy_packet mode\n");
 	if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
 		fprintf(stderr, "can't set packet_copy mode\n");
 		exit(1);
