3b341b9f6ebfdd3aaab19e019934884fd2f77e07
lsj <jaef77@naver.com>
2018-10-03 07:55:12 +0900

    Edit errors

--- nfqnl_test.c
+++ nfqnl_test.c
@@ -39,49 +39,23 @@ static struct id_acc print_pkt (struct nfq_data *tb)
 	unsigned char *data;
 
 	ph = nfq_get_msg_packet_hdr(tb);
-	/*
 	if (ph) {
 		id = ntohl(ph->packet_id);
-		printf("hw_protocol=0x%04x hook=%u id=%u ", ntohs(ph->hw_protocol), ph->hook, id);
 	}
-	*/
 
 	hwph = nfq_get_packet_hw(tb);
-	/*
-	if (hwph) {
-		int i, hlen = ntohs(hwph->hw_addrlen);
-
-		printf("hw_src_addr=");
-		for (i = 0; i < hlen-1; i++)
-			printf("%02x:", hwph->hw_addr[i]);
-		printf("%02x ", hwph->hw_addr[hlen-1]);
-	}
-	*/
-
 
 	mark = nfq_get_nfmark(tb);
-	// if (mark)
-	// 	printf("mark=%u ", mark);
 
 	ifi = nfq_get_indev(tb);
-	// if (ifi)
-	//	printf("indev=%u ", ifi);
 
 	ifi = nfq_get_outdev(tb);
-	// if (ifi)
-	//	printf("outdev=%u ", ifi);
 
 	ifi = nfq_get_physindev(tb);
-	//if (ifi)
-	//	printf("physindev=%u ", ifi);
 
 	ifi = nfq_get_physoutdev(tb);
-	//if (ifi)
-	//	printf("physoutdev=%u ", ifi);
 
 	ret = nfq_get_payload(tb, &data);
-	// if (ret >= 0)
-	//	printf("payload_len=%d ", ret);
 
 	// fputc('\n', stdout);
 
@@ -109,22 +83,20 @@ static struct id_acc print_pkt (struct nfq_data *tb)
 					{
 						if(memcmp(&tmp_data[i], "Host: ", 6) == 0)
 						{
-							int z=6;
-							while(memcmp(&tmp_data[i+z], "\r\n", 2) != 0)
-								z++;
-							for(int j=6;j<z;j++)
+							int j=6;
+							while(1)
 							{
 								if(memcmp(&tmp_data[i+j], host_name, host_len) == 0)
 								{
 									printf("FORBIDDEN ACCESS DETECTED!\n");
-									dump(data, ret);
 									struct id_acc tmp;
 									tmp.id = id;
 									tmp.acc = 0;
 									return tmp;
 								}
-								else
-									continue;
+								else if(tmp_data[i+j] == '\n')
+									break;
+								j++;
 							}
 							break;
 						} // if(Host: )
@@ -164,6 +136,7 @@ int main(int argc, char **argv)
 
 	host_name = argv[1];
 	host_len = strlen(host_name);
+	printf("%s, %d\n", host_name, host_len);
 
 	printf("opening library handle\n");
 	h = nfq_open();
